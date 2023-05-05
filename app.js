ce7375f0c68ced89416547c17ceb74ed7bc9d1d0
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-05-05 18:42:52 +0200

    Fungerende lommeregner lavet af Chat GPT

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
 "use strict"
 
-window.addEventListener("load", initApp);
+const buttons = document.querySelectorAll('.calculator input[type="button"]'); // Vælg alle knapper
 
-function initApp() {
-    console.log("JS kører");
+buttons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    const value = button.value;
+
+    if (value === "AC") {
+      clearDisplay();
+    } else if (value === "DE") {
+      deleteChar();
+    } else if (value === "=") {
+      calculateResult();
+    } else {
+      addCharToDisplay(value);
+    }
+  });
+});
+
+function clearDisplay() {
+  document.querySelector("#display").value = "";
 }
 
-function addNumbers() {
-    
+function deleteChar() {
+  const display = document.querySelector("#display");
+  const value = display.value;
+  display.value = value.slice(0, -1);
+}
+
+function addCharToDisplay(char) {
+  document.querySelector("#display").value += char;
+}
+
+function calculateResult() {
+  const display = document.querySelector("#display");
+  const result = eval(display.value);
+  display.value = result;
 }
